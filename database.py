be28b70cf36403696f73433dd7f052f4b8b844f8
BraveCake <aliotaku241@gmail.com>
2022-04-25 01:16:11 +0200

    migration is done

--- database.py
+++ database.py
@@ -11,9 +11,9 @@ class Database():
     Database.me.commit()
   def __getitem__(self, key):
     self.fdb.execute("SELECT value FROM main WHERE key=%s",(key,))
-    return self.fdb.fetchone()
+    return self.fdb.fetchone()[0]
   def __setitem__(self,key,value):
     self.fdb.execute("INSERT INTO main (key,value) VALUES(%s,%s) ON CONFLICT (key) DO UPDATE SET value =%s",(key,value,value))
   def keys(self):
     self.fdb.execute('SELECT key FROM main')
-    return self.fdb.fetchall()
+    return self.fdb.fetchall()[0]
