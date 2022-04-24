02cf07207f381fc20fd44d8b55df3836e15eb7cd
BraveCake <aliotaku241@gmail.com>
2022-04-24 20:03:19 +0200

    moving to postgre

--- database.py
+++ database.py
@@ -10,8 +10,8 @@ class Database():
     self.fdb= Database.me.cursor()
 
   def __getitem__(self, key):
-        return self.fdb.excute('SELECT value FROM main WHERE key='+key).fetchone()
+        return self.fdb.execute('SELECT value FROM main WHERE key='+key).fetchone()
   def __setitem__(self,key,value):
-    self.fdb.excute('INSERT INTO main (key,value) VALUES('+key+','+value+') ON DUPLICATE KEY UPDATE key='+key+' value ='+value)
+    self.fdb.execute('INSERT INTO main (key,value) VALUES('+key+','+value+') ON DUPLICATE KEY UPDATE key='+key+' value ='+value)
   def keys(self):
-    return self.fdb.excute('SELECT key FROM main').fetchone().fetchall()
+    return self.fdb.execute('SELECT key FROM main').fetchone().fetchall()
