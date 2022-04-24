1d65a048284df67097b278e35188f4d49007290c
BraveCake <aliotaku241@gmail.com>
2022-04-24 19:57:44 +0200

    moving to postgre

--- database.py
+++ database.py
@@ -1,19 +1,16 @@
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
+import psycopg2
+
 class Database():
   me = None
   def __init__(self):
     if Database.me !=None:
       raise Exception('Instance of database already exists!')
-    cred = credentials.Certificate("key.json")
-    firebase_admin.initialize_app(cred)
-    self.fdb= firestore.client()
+    Database.me= psycopg2.connect(os.environ['DATABASE_URL'], sslmode='require')
+    self.fdb= Database.me.cursor()
+
   def __getitem__(self, key):
-        return self.fdb.collection(u'mainc').document(key).get().to_dict()['value']
+        return self.fdb.excute('SELECT value FROM main WHERE key='+key).fetchone()
   def __setitem__(self,key,value):
-    if(key=='__apps__'):
-      key='__appss__'
-    self.fdb.collection(u'mainc').document(key).set({"value":value})
+    self.fdb.excute('INSERT INTO main (key,value) VALUES('+key+','+value+') ON DUPLICATE KEY UPDATE key='+key+' value ='+value)
   def keys(self):
-    return map(lambda doc : doc.id,self.fdb.collection(u'mainc').get())
+    return self.fdb.excute('SELECT key FROM main').fetchone().fetchall()
