b75370b67d98f1a8d77e906c52ba5eb248478684
BraveCake <aliotaku241@gmail.com>
2022-04-25 01:08:30 +0200

    exported data from old db

--- database.py
+++ database.py
@@ -1,5 +1,4 @@
 import psycopg2
-import os
 
 class Database():
   me = None
@@ -7,11 +6,14 @@ class Database():
     if Database.me !=None:
       raise Exception('Instance of database already exists!')
     Database.me= psycopg2.connect(os.environ['DATABASE_URL'], sslmode='require')
+    Database.me.autocommit = True
     self.fdb= Database.me.cursor()
-
+    Database.me.commit()
   def __getitem__(self, key):
-        return self.fdb.execute("SELECT value FROM main WHERE key=%s",(key,)).fetchone()
+    self.fdb.execute("SELECT value FROM main WHERE key=%s",(key,))
+    return self.fdb.fetchone()
   def __setitem__(self,key,value):
     self.fdb.execute("INSERT INTO main (key,value) VALUES(%s,%s) ON CONFLICT (key) DO UPDATE SET value =%s",(key,value,value))
   def keys(self):
-    return self.fdb.execute('SELECT key FROM main').fetchall()
+    self.fdb.execute('SELECT key FROM main')
+    return self.fdb.fetchall()
