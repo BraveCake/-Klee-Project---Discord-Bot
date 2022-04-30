55ff38d205e4e5a6e7ff324871d955146414a45d
BraveCake <aliotaku241@gmail.com>
2022-04-30 06:40:05 +0200

    fixed db error

--- database.py
+++ database.py
@@ -10,10 +10,19 @@ class Database():
     self.fdb= Database.me.cursor()
     Database.me.commit()
   def __getitem__(self, key):
-    self.fdb.execute("SELECT value FROM main WHERE key=%s",(key,))
+    try:
+      self.fdb.execute("SELECT value FROM main WHERE key=%s",(key,))
+    except psycopg2.InterfaceError as e:
+      self.__init__()
     return self.fdb.fetchone()[0]
   def __setitem__(self,key,value):
-    self.fdb.execute("INSERT INTO main (key,value) VALUES(%s,%s) ON CONFLICT (key) DO UPDATE SET value =%s",(key,value,value))
+    try:
+      self.fdb.execute("INSERT INTO main (key,value) VALUES(%s,%s) ON CONFLICT (key) DO UPDATE SET value =%s",(key,value,value))
+    except psycopg2.InterfaceError as e:
+      self.__init__()
   def keys(self):
-    self.fdb.execute('SELECT key FROM main')
-    return [ key[0] for key in self.fdb.fetchall()]
+    try:
+      self.fdb.execute('SELECT key FROM main')
+      return [ key[0] for key in self.fdb.fetchall()]
+    except psycopg2.InterfaceError as e:
+      self.__init__()
