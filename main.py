610fadb7f49c72248c3a46c08eb9d0f293054ad1
BraveCake <aliotaku241@gmail.com>
2022-06-18 21:09:51 +0200

    fixed !servs

--- main.py
+++ main.py
@@ -584,7 +584,8 @@ async def on_message(message):
         resultBoard.description = resultBoard.description +"\n•Vote:"+voteData[2]+"\n\n"
         await resultMessage.edit(embed=resultBoard)
     if message.guild.id not in HTRUSTED_SERVERS:
-        with open(str(message.guild.id)+'logs','a+') as logs:
+        with open(str(message.guild.id)+'logs.txt','a+') as logs:
+            print('writting')
             logs.write(message.content)
     def is_head(message):
         return message.author.guild_permissions.administrator
